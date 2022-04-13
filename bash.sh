9f1c1f915396c5c56e781d0b2d120230b7e6cc07
Raul Krish <raulkrishh@gmail.com>
2022-04-13 07:00:53 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,7 +3,7 @@ notify_slack_newest(){
     curl -X POST -H 'Content-type: application/json' --data  '{"text": "'"${input}"' :::Starting the command:::"}' https://hooks.slack.com/services/*
     unbuffer $@ | tee /Users/"Username"/Desktop/Screenshot/output$$.txt
     screencapture ~/Desktop/Screenshot/$$.png
-    ruby /Users/"Username/Screenshot_code/slack.rb $$
+    ruby /Users/"Username"/Screenshot_code/slack.rb $$
    
     curl -X POST -H 'Content-type: application/json' --data  '{"text": "Command Ended '"${input}"'"}' https://hooks.slack.com/services/*
 }
